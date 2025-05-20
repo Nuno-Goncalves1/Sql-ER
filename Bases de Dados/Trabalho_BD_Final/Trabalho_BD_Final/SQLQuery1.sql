bf82de83b497a711bc235546c41267a7195a427c
Nuno-Goncalves1 <nr.salsa2006@gmail.com>
2025-05-20 11:31:44 +0100

    modified:   Bases de Dados/Trabalho_BD_Final/Trabalho_BD_Final/SQLQuery1.sql

--- Bases de Dados/Trabalho_BD_Final/Trabalho_BD_Final/SQLQuery1.sql
+++ Bases de Dados/Trabalho_BD_Final/Trabalho_BD_Final/SQLQuery1.sql
@@ -1,96 +1,4 @@
-CREATE TABLE Municipe (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(100)
-);
 
-CREATE TABLE Entidade (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(100)
-);
-
-CREATE TABLE Proposta (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    descricao TEXT,
-    id_ INT,
-    FOREIGN KEY (id_) REFERENCES(id)
-);
-
-CREATE TABLE Parecer (
-    id_entidade INT,
-    id_proposta INT,
-    CONSTRAINT PK_Parecer PRIMARY KEY (id_entidade, id_proposta),
-    FOREIGN KEY (id_entidade) REFERENCES Entidade(id),
-    FOREIGN KEY (id_proposta) REFERENCES Proposta(id)
-);
-
-CREATE TABLE EstudoViabilidade (
-    id INT PRIMARY KEY,
-    detalhes TEXT,
-    FOREIGN KEY (id) REFERENCES Proposta(id)
-);
-
-CREATE TABLE Analise (
-    id INT PRIMARY KEY,
-    detalhes TEXT,
-    FOREIGN KEY (id) REFERENCES Proposta(id)
-);
-
-CREATE TABLE FaseExecucao (
-    id INT PRIMARY KEY,
-    detalhes TEXT,
-    FOREIGN KEY (id) REFERENCES Proposta(id)
-);
-
-CREATE TABLE EmOrcamento (
-    id INT PRIMARY KEY,
-    detalhes TEXT,
-    FOREIGN KEY (id) REFERENCES Proposta(id)
-);
-
-CREATE TABLE AnaliseExecucao (
-    id INT PRIMARY KEY,
-    detalhes TEXT,
-    FOREIGN KEY (id) REFERENCES Proposta(id)
-);
-
-CREATE TABLE Construtora (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(100)
-);
-
-CREATE TABLE Orcamento (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    id_proposta INT,
-    id_construtora INT,
-    FOREIGN KEY (id_proposta) REFERENCES Proposta(id),
-    FOREIGN KEY (id_construtora) REFERENCES Construtora(id)
-);
-
-CREATE TABLE Materiais (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(100)
-);
-
-CREATE TABLE Maquinas (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(100)
-);
-
-CREATE TABLE OrcamentoMateriais (
-    id_orcamento INT,
-    id_material INT,
-    PRIMARY KEY (id_orcamento, id_material),
-    FOREIGN KEY (id_orcamento) REFERENCES Orcamento(id),
-    FOREIGN KEY (id_material) REFERENCES Materiais(id)
-);
-
-CREATE TABLE OrcamentoMaquinas (
-    id_orcamento INT,
-    id_maquina INT,
-    PRIMARY KEY (id_orcamento, id_maquina),
-    FOREIGN KEY (id_orcamento) REFERENCES Orcamento(id),
-    FOREIGN KEY (id_maquina) REFERENCES Maquinas(id)
-);
 -- Key's
 
 -- Consultas para verificar a estrutura das tabelas criadas
@@ -155,8 +63,9 @@ INNER JOIN sys.tables t ON fk.parent_object_id = t.object_id
 INNER JOIN sys.schemas s ON t.schema_id = s.schema_id;
 
 EXEC sp_executesql @sql;
+-- Exclui as chaves estrangeiras
 
-
+--Exclusao de tabelas
 DROP TABLE  OrcamentoMaquinas;
 DROP TABLE  OrcamentoMateriais;
 DROP TABLE  Orcamento;
