82d479f3977ee59c5c12bf33cb1a460f0b96f6a2
Game Jog <baptistaafonso153@gmail.com>
2025-05-20 12:05:08 +0100

    Pesquisas

--- Bases de Dados/Trabalho_BD_Final/Trabalho_BD_Final/SQLQuery1.sql
+++ Bases de Dados/Trabalho_BD_Final/Trabalho_BD_Final/SQLQuery1.sql
@@ -35,6 +35,32 @@ SELECT *
 FROM OrcamentoMaquinas;
 
 
+/* 1.1 B) */
+Select * From Municipe As m Inner Join Proposta as p On m.idMunicipe = p.idMunicipe
+Where m.nome Like '%Rita%' And p.data Between '2024-04-01' And '2024-06-30' Order By m.nome Desc;
+
+/* 1.1 C)*/
+
+Select m.nome, Count(p.idProp) As 'Quantidade de Propostas' 
+From Municipe as m Left Join Proposta as p On m.idMunicipe = p.idMunicipe 
+Group By m.nome Order By Count(p.idProp) Desc;
+
+/* 1.1 D)*/
+
+Select m.nome
+From Municipe as m Left Join Proposta as p On m.idMunicipe = p.idMunicipe 
+Group By m.nome
+Having Count(p.idProposta) = 0 
+Order By m.nome;
+
+/* 1.1 E) */
+
+Select p.nome, Avg(p.avaliacao) From Proposta As p
+Where p.data Between '2024-01-01' And '2024-03-31' 
+Group By p.nome
+Having Count(p.avaliacao) > 10 And Avg(p.avaliacao) >= 8 
+Order By Avg(p.avaliacao) Desc;
+
 SELECT TABLE_SCHEMA, TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_TYPE = 'BASE TABLE';
@@ -59,7 +85,7 @@ DECLARE @sql NVARCHAR(MAX) = N'';
 SELECT @sql += '
 ALTER TABLE [' + s.name + '].[' + t.name + '] DROP CONSTRAINT [' + fk.name + '];'
 FROM sys.foreign_keys fk
-INNER JOIN sys.tables t ON fk.parent_object_id = t.object_id
+INNER JOIN sys.tables t ON fk.parent_object_id = t.object_id    
 INNER JOIN sys.schemas s ON t.schema_id = s.schema_id;
 
 EXEC sp_executesql @sql;
