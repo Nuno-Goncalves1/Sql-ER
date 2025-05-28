a7594f311b3deef1b5f10075e5d0a0d402efa84c
DilanPereira <dilanmpereira2002@gmail.com>
2025-05-28 12:36:49 +0100

    novas tabelas(incompleto)

--- Bases de Dados/Trabalho_BD_Final/Trabalho_BD_Final/SQLQuery1.sql
+++ Bases de Dados/Trabalho_BD_Final/Trabalho_BD_Final/SQLQuery1.sql
@@ -18,11 +18,6 @@ CREATE TABLE Proposta (
 	titulo VARCHAR(MAX),
 	descricao VARCHAR(MAX),
 	data DATE,
-	fase VARCHAR(30),
-	inicioFase DATE,
-	fimFase DATE,
-	montante INT,
-	percentagemCoima INT CHECK (percentagemCoima >= 0 AND percentagemCoima <= 100),
     CONSTRAINT PK_Proposta PRIMARY KEY (idProposta)
 );
 CREATE TABLE Classifica(
@@ -73,49 +68,6 @@ CREATE TABLE Participa(
     CONSTRAINT PK_Participa PRIMARY KEY (idConstrutora,idConsorcio)
 );
 
-CREATE TABLE Orcamento(
-    idOrcamento INT UNIQUE,
-	idProposta INT, --FK
-	designacao VARCHAR(MAX),
-	valor INT CHECK (valor > 0),
-	dataInicioExecucao DATE,
-	dataFimExecucao DATE,
-    CONSTRAINT PK_Orcamento PRIMARY KEY (idOrcamento, idProposta)
-);
-CREATE TABLE OrcamentoConstrutora(
-	idOrcamento INT, --FK
-	idConstrutora INT, --FK
-	CONSTRAINT PK_OrcamentoConstrutora PRIMARY KEY (idOrcamento, idConstrutora)
-);
-CREATE TABLE OrcamentoConsorcio(
-	idOrcamento INT, --FK
-	idConsorcio INT, --FK
-	CONSTRAINT PK_OrcamentoConsorcio PRIMARY KEY (idOrcamento, idConsorcio)
-);
-
-CREATE TABLE Materiais(
-    idMateriais INT,
-	designacao VARCHAR(MAX),
-    CONSTRAINT PK_Materiais PRIMARY KEY (idMateriais)
-);
-CREATE TABLE PrecisaMaterial(
-	idOrcamento INT, --FK
-	idMateriais INT, --FK
-    CONSTRAINT PK_PrecisaMaterial PRIMARY KEY (idOrcamento,idMateriais)
-);
-
-CREATE TABLE Equipamentos(
-    idEquipamentos INT,
-	designacao VARCHAR(MAX),
-	horasUtilizacao INT CHECK( horasUtilizacao > 0 AND horasUtilizacao <= 24),
-	dataAquisicao DATE,
-    CONSTRAINT PK_Equipamentos PRIMARY KEY (idEquipamentos)
-);
-CREATE TABLE PrecisaEquipamento(
-	idOrcamento INT, --FK
-	idEquipamentos INT, --FK
-    CONSTRAINT PK_PrecisaEquipamento PRIMARY KEY (idOrcamento,idEquipamentos)
-);
 
 CREATE TABLE EstudoViabilidade(
     idEstudo INT,
@@ -154,7 +106,7 @@ CREATE TABLE FaseExecucao(
 	concluida BIT NOT NULL DEFAULT 0, -- 0 false, 1 true
 	relatorio VARCHAR(MAX),
 	dataConclusao DATE,
-    CONSTRAINT PK_FaseExecucao PRIMARY KEY (idFase)
+    CONSTRAINT PK_FaseExecucao PRIMARY KEY (idFase, idProposta)
 );
 CREATE TABLE EmOrcamento(
     idEmOrcamento INT,
@@ -178,6 +130,61 @@ CREATE TABLE AnaliseExecucao(
     CONSTRAINT PK_AnaliseExecucao PRIMARY KEY (idAnaliseExecucao,idProposta)
 );
 
+
+CREATE TABLE Orcamento(
+    idOrcamento INT UNIQUE,
+	idFaseExecucao INT, --FK
+	valor INT CHECK (valor > 0),
+	dataInicioExecucao DATE,
+	dataFimExecucao DATE,
+    CONSTRAINT PK_Orcamento PRIMARY KEY (idOrcamento, idFaseExecucao)
+);
+
+CREATE TABLE OrdensDeTrabalho(
+	idOrdemDeTrabalho INT,
+	idOrcamento INT, --FK
+	designacao VARCHAR(MAX),
+	dataInicio DATE,
+	dataFim DATE,
+	CONSTRAINT PK_OrdemDeTrabalho PRIMARY KEY (idOrdemDeTrabalho)
+);
+
+CREATE TABLE OrcamentoConstrutora(
+	idOrcamento INT, --FK
+	idConstrutora INT, --FK
+	CONSTRAINT PK_OrcamentoConstrutora PRIMARY KEY (idOrcamento, idConstrutora)
+);
+CREATE TABLE OrcamentoConsorcio(
+	idOrcamento INT, --FK
+	idConsorcio INT, --FK
+	CONSTRAINT PK_OrcamentoConsorcio PRIMARY KEY (idOrcamento, idConsorcio)
+);
+
+CREATE TABLE Materiais(
+    idMateriais INT,
+	designacao VARCHAR(MAX),
+    CONSTRAINT PK_Materiais PRIMARY KEY (idMateriais)
+);
+CREATE TABLE PrecisaMaterial(
+	idOrdemDeTrabalho INT, --FK
+	idMateriais INT, --FK
+    CONSTRAINT PK_PrecisaMaterial PRIMARY KEY (idOrdemDeTrabalho,idMateriais)
+);
+
+CREATE TABLE Equipamentos(
+    idEquipamentos INT,
+	designacao VARCHAR(MAX),
+	horasUtilizacao INT CHECK( horasUtilizacao > 0 AND horasUtilizacao <= 24),
+	dataAquisicao DATE,
+    CONSTRAINT PK_Equipamentos PRIMARY KEY (idEquipamentos)
+);
+CREATE TABLE PrecisaEquipamento(
+	idOrdemDeTrabalho INT, --FK
+	idEquipamentos INT, --FK
+    CONSTRAINT PK_PrecisaEquipamento PRIMARY KEY (idOrdemDeTrabalho,idEquipamentos)
+);
+
+
 -- Foreign Key's
 ALTER TABLE Proposta
 ADD CONSTRAINT FK_Proposta_Municipe FOREIGN KEY (idMunicipe) REFERENCES Municipe (idMunicipe);
@@ -194,17 +201,6 @@ ALTER TABLE Participa
 ADD CONSTRAINT FK_Participa_Construtora FOREIGN KEY (idConstrutora) REFERENCES Construtora (idConstrutora),
 CONSTRAINT FK_Participa_Consorcio FOREIGN KEY (idConsorcio) REFERENCES Consorcio (idConsorcio);
 
-ALTER TABLE Orcamento
-ADD CONSTRAINT FK_Orcamento_Proposta FOREIGN KEY (idProposta) REFERENCES Proposta (idProposta);
-
-ALTER TABLE OrcamentoConstrutora
-ADD CONSTRAINT FK_OrcamentoConstrutora_Orcamento FOREIGN KEY (idOrcamento) REFERENCES Orcamento (idOrcamento),
-CONSTRAINT FK_OrcamentoConstrutora_Construtora FOREIGN KEY (idConstrutora) REFERENCES Construtora (idConstrutora); 
-
-ALTER TABLE OrcamentoConsorcio
-ADD CONSTRAINT FK_OrcamentoConsorcio_Orcamento FOREIGN KEY (idOrcamento) REFERENCES Orcamento (idOrcamento),
-CONSTRAINT FK_OrcamentoConsorcio_Consorcio FOREIGN KEY (idConsorcio) REFERENCES Consorcio (idConsorcio);
-
 ALTER TABLE EstudoViabilidade
 ADD CONSTRAINT FK_EstudoViabilidade_Proposta FOREIGN KEY (idProposta) REFERENCES Proposta (idProposta);
 
@@ -220,13 +216,38 @@ ADD CONSTRAINT FK_EmOrcamento_Proposta FOREIGN KEY (idProposta) REFERENCES Propo
 ALTER TABLE AnaliseExecucao
 ADD CONSTRAINT FK_AnaliseExecucao_Proposta FOREIGN KEY (idProposta) REFERENCES Proposta (idProposta);
 
+ALTER TABLE Orcamento
+ADD CONSTRAINT FK_Orcamento_FaseExecucao FOREIGN KEY (idFaseExecucao) REFERENCES FaseExecucao (idFase);
+
+ALTER TABLE OrcamentoConstrutora
+ADD CONSTRAINT FK_OrcamentoConstrutora_Orcamento FOREIGN KEY (idOrcamento) REFERENCES Orcamento (idOrcamento),
+CONSTRAINT FK_OrcamentoConstrutora_Construtora FOREIGN KEY (idConstrutora) REFERENCES Construtora (idConstrutora); 
+
+ALTER TABLE OrcamentoConsorcio
+ADD CONSTRAINT FK_OrcamentoConsorcio_Orcamento FOREIGN KEY (idOrcamento) REFERENCES Orcamento (idOrcamento),
+CONSTRAINT FK_OrcamentoConsorcio_Consorcio FOREIGN KEY (idConsorcio) REFERENCES Consorcio (idConsorcio);
+
+ALTER TABLE OrdensDeTrabalho
+ADD CONSTRAINT FK_OrdemDeTrabalho_Orcamento FOREIGN KEY (idOrcamento) REFERENCES Orcamento (idOrcamento);
+
+ALTER TABLE PrecisaMaterial
+ADD CONSTRAINT FK_PrecisaMateriais_OrdemDeTrabalho FOREIGN KEY (idOrdemDeTrabalho) REFERENCES OrdensDeTrabalho (idOrdemDeTrabalho),
+CONSTRAINT FK_PrecisaMateriais_Materiais FOREIGN KEY (idMateriais) REFERENCES Materiais (idMateriais);
+
+ALTER TABLE PrecisaEquipamento
+ADD CONSTRAINT FK_PrecisaEquipamento_OrdemDeTrabalho FOREIGN KEY (idOrdemDeTrabalho) REFERENCES OrdensDeTrabalho (idOrdemDeTrabalho),
+CONSTRAINT FK_PrecisaEquipamento_Equipamento FOREIGN KEY (idEquipamentos) REFERENCES Equipamentos (idEquipamentos);
+
+
+
 -- Inserir os dados
 INSERT INTO Municipe VALUES 
 (1, 'Afonso Baptista', 193284761, '123456789ZZ9', '1990-04-15', 'M', 'Rua das Flores, Lisboa'),
 (2, 'Dilan Pereira', 256743829, '987654321AA1', '1985-11-30', 'M', 'Avenida Central, Porto'),
 (3, 'Nuno Gonçalves', 312567984, '192837465BC2', '1978-06-22', 'M', 'Travessa do Norte, Coimbra'),
-(4, 'Mariana Lopes', 134859267, '564738291DC3', '1995-08-09', 'F', 'Rua do Carmo, Braga'),
-(5, 'Inês Martins', 276198435, '918273645XY4', '1988-03-25', 'F', 'Estrada Velha, Faro');
+(4, 'Rita Lopes', 134859267, '564738291DC3', '1995-08-09', 'F', 'Rua do Carmo, Braga'),
+(5, 'Inês Martins', 276198435, '918273645XY4', '1988-03-25', 'F', 'Estrada Velha, Faro'),
+(6, 'Rita Silva', 987654321, '123456789AB1', '1996-03-15', 'F', 'Avenida Central, Braga');
 
 
 INSERT INTO Entidade(idEntidade,nome,nipc,morada,email,telefone) VALUES
@@ -234,6 +255,10 @@ INSERT INTO Entidade(idEntidade,nome,nipc,morada,email,telefone) VALUES
 (2,'Animais',509274631,'Rua do Leão','animais@natureza.pt',938724560),
 (3,'Natureza',508392714,'Rua da Natureza','geral@natureza.pt',961234567),
 (4,'Tráfego',509482376,'Avenida dos Transportes','geral@transportes.pt',939876543);
+
+INSERT INTO Proposta(idProposta, idMunicipe, titulo, descricao, data, fase, inicioFase, fimFase, montante, percentagemCoima) VALUES
+(1, 4, 'Requalificação de Parque Urbano', 'Proposta para renovar e modernizar o parque urbano do centro da cidade, incluindo novas zonas verdes e iluminação pública.', 
+'2025-05-28', NULL, NULL, NULL, 50000.00, 5.0);
 
 -- Acréscimos tendo em conta a ‘Proporcionalidade de Grupo (a)
 
@@ -422,11 +447,12 @@ ALTER TABLE Parecer DROP CONSTRAINT FK_Parecer_Entidade;
 ALTER TABLE Parecer DROP CONSTRAINT FK_Parecer_Proposta;
 ALTER TABLE Participa DROP CONSTRAINT FK_Participa_Construtora;
 ALTER TABLE Participa DROP CONSTRAINT FK_Participa_Consorcio;
-ALTER TABLE Orcamento DROP CONSTRAINT FK_Orcamento_Proposta;
-ALTER TABLE OrcamentoConstrutora DROP CONSTRAINT FK_OrcamentoConstrutora_Orcamento;
-ALTER TABLE OrcamentoConstrutora DROP CONSTRAINT FK_OrcamentoConstrutora_Construtora;
-ALTER TABLE OrcamentoConsorcio DROP CONSTRAINT FK_OrcamentoConsorcio_Orcamento;
-ALTER TABLE OrcamentoConsorcio DROP CONSTRAINT FK_OrcamentoConsorcio_Consorcio;
+
+ALTER TABLE AnaliseExecucao DROP CONSTRAINT FK_AnaliseExecucao_Proposta;
+ALTER TABLE EmOrcamento DROP CONSTRAINT FK_EmOrcamento_Proposta;
+ALTER TABLE FaseExecucao DROP CONSTRAINT FK_FaseExecucao_Proposta;
+ALTER TABLE EstudoViabilidade DROP CONSTRAINT FK_EstudoViabilidade_Proposta;
+ALTER TABLE AnaliseMunipes DROP CONSTRAINT FK_AnaliseMunipes_Proposta;
 
 DROP TABLE Municipe;
 DROP TABLE Proposta;
